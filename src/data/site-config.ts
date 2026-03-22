e45d7e4380d4661d209c2987d413b8632b00502f
Dheiry Yojaira Lopez Santana <79459469+dheirylopez@users.noreply.github.com>
2026-03-22 11:29:07 -0400

    volviendo al principio

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -1,19 +1,20 @@
-
+import avatar from '/src/assets/images/avatar.jpg';
+import hero from '/src/assets/images/hero.jpg';
 import type { SiteConfig } from '../types';
 
 const siteConfig: SiteConfig = {
     website: 'https://dheirylopez.github.io',
  avatar: {
-        src: '/images/avatar.jpg',
-        alt: ''
+        src: avatar,
+        alt: 'Ethan Donovan'
     },
     title: 'Dheiry Lopez',
     subtitle: 'Minimal Astro.js theme',
     description: 'portafolio Dheiry',
-       image: {
-                      src: 'src/assets/images/avatar.jpg',
-                      alt: 'Banner principal del portafolio de Dheiry Lopez'
-                   },
+        image: {
+        src: '/dante-preview.jpg',
+        alt: 'Dante - Astro.js and Tailwind CSS theme'
+    },
     headerNavLinks: [
         {
             text: 'Home',
@@ -56,10 +57,10 @@ const siteConfig: SiteConfig = {
     hero: {
       title: 'Hello & Welcome! Explore My Software Projects!',
         text: 'Hi, I’m Dheiry López, a Senior Software Engineer with 7+ years of experience delivering scalable software solutions and supporting cross-functional project execution.<br /><br /> My background includes designing and developing enterprise applications, building RESTful APIs, contributing to microservices architectures, supporting AWS-based environments, and developing RPA bots with Automation Anywhere to streamline repetitive business processes and increase operational efficiency. <br /><br />I’ve worked across industries including energy, HR/payroll, healthcare manufacturing, and technology, which has given me the adaptability to understand new domains quickly and develop practical solutions for diverse business needs. <br /> <br />Today, I’m focused on helping companies and clients build software and automation solutions that are clean, maintainable, user-centered, and capable of driving long-term impact.',
-             image: {
-                      src: 'src/assets/images/hero.jpg',
-                      alt: 'Banner principal del portafolio de Dheiry Lopez'
-                   },
+           image: {
+            src: hero,
+            alt: 'A person sitting at a desk in front of a computer'
+        },
         actions: [
             {
                 text: 'Get in Touch',
